be19dc2f0bf61a248211c451a00bc9aa751397b7
fido <fido@FIDO-CVUT>
2016-10-04 11:07:03 +0200

    Demand simulation starter added, new random color visualisation layer

--- src/main/java/com/mycompany/testsim/DemandSimulation.java
+++ src/main/java/com/mycompany/testsim/DemandSimulation.java
@@ -2,13 +2,82 @@
  */
 package com.mycompany.testsim;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.mycompany.testsim.io.Trip;
+import com.mycompany.testsim.io.TripTransform;
+import cz.agents.agentpolis.simmodel.environment.StandardAgentPolisModule;
+import cz.agents.agentpolis.simmodel.environment.model.delaymodel.impl.InfinityDelayingSegmentCapacityDeterminer;
+import cz.agents.agentpolis.simulator.creator.SimulationCreator;
+import cz.agents.agentpolis.utils.config.ConfigReader;
+import cz.agents.agentpolis.utils.config.ConfigReaderException;
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author F-I-D-O
  */
 public class DemandSimulation {
 	
-	public static void main(String[] args) {
-		
+	private static final String EXPERIMENT_PATH = "data/Prague";
+	
+	private static final String INPUT_FILE_PATH = "data/Prague/trips.json";
+	
+	private static final int SRID = 2065;
+    
+    
+    
+	
+	public static void main(String[] args) throws MalformedURLException, ConfigReaderException {
+		new DemandSimulation().run();
+	}
+    
+    
+    
+	
+	public void run() throws ConfigReaderException{
+		try {
+			List<Trip<Long>> osmNodesList = TripTransform.jsonToTrips(new File(INPUT_FILE_PATH), Long.class);
+			
+			File experimentDir = new File(EXPERIMENT_PATH);
+
+			ConfigReader scenario = ConfigReader.initConfigReader(new File(experimentDir, "scenario.groovy").toURI().toURL());
+			MyParams parameters = new MyParams(experimentDir, scenario);
+			SimpleEnvinromentFactory envinromentFactory = new SimpleEnvinromentFactory(new InfinityDelayingSegmentCapacityDeterminer());
+
+			
+			Injector injector = Guice.createInjector(new MainModule(envinromentFactory, parameters));
+			
+
+//			SimulationCreator creator = new SimulationCreator(
+//					new SimpleEnvinromentFactory(new InfinityDelayingSegmentCapacityDeterminer()), parameters);
+			
+			SimulationCreator creator = injector.getInstance(SimulationCreator.class);
+			
+			creator.setMainEnvironment(injector);
+
+	//		creator.addInitModuleFactory(new VehicleDataModelFactory(parameters.vehicleDataModelFile));
+
+	//        creator.addAgentInit(new MyAgentInitFactory());
+	
+			creator.addInitFactory(new DemendsInitFactory(osmNodesList, creator));
+
+			// set up visual appearance of agents
+			creator.addEntityStyleVis(DemandSimulationEntityType.DEMAND, Color.GREEN, 8);
+
+			// start it up
+			creator.startSimulation(new MyMapInitFactory(SRID));
+
+			System.exit(0);
+			
+		} catch (IOException ex) {
+			Logger.getLogger(PrecomputedDemandSimulation.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 }
---
+++ src/main/java/com/mycompany/testsim/visio/DemandsVisioInItializer.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testsim.visio;
+
+import com.google.inject.Inject;
+import cz.agents.agentpolis.simmodel.entity.AgentPolisEntity;
+import cz.agents.agentpolis.simmodel.environment.model.AgentPositionModel;
+import cz.agents.agentpolis.simmodel.environment.model.AgentStorage;
+import cz.agents.agentpolis.simmodel.environment.model.EntityStorage;
+import cz.agents.agentpolis.simmodel.environment.model.VehiclePositionModel;
+import cz.agents.agentpolis.simmodel.environment.model.VehicleStorage;
+import cz.agents.agentpolis.simmodel.environment.model.citymodel.transportnetwork.AllNetworkNodes;
+import cz.agents.agentpolis.simmodel.environment.model.citymodel.transportnetwork.BikewayNetwork;
+import cz.agents.agentpolis.simmodel.environment.model.citymodel.transportnetwork.HighwayNetwork;
+import cz.agents.agentpolis.simmodel.environment.model.citymodel.transportnetwork.MetrowayNetwork;
+import cz.agents.agentpolis.simmodel.environment.model.citymodel.transportnetwork.PedestrianNetwork;
+import cz.agents.agentpolis.simmodel.environment.model.citymodel.transportnetwork.RailwayNetwork;
+import cz.agents.agentpolis.simmodel.environment.model.citymodel.transportnetwork.TramwayNetwork;
+import cz.agents.agentpolis.simulator.creator.SimulationCreator;
+import cz.agents.agentpolis.simulator.visualization.visio.Projection;
+import cz.agents.agentpolis.simulator.visualization.visio.entity.DefaultVisioInitializer;
+import cz.agents.alite.simulation.Simulation;
+import cz.agents.alite.vis.VisManager;
+
+/**
+ *
+ * @author fido
+ */
+public class DemandsVisioInItializer extends DefaultVisioInitializer{
+    
+    private final DemandVisualisationLayer demandVisualisationLayer;
+    
+    @Inject
+    public DemandsVisioInItializer(PedestrianNetwork pedestrianNetwork, BikewayNetwork bikewayNetwork, 
+            HighwayNetwork highwayNetwork, TramwayNetwork tramwayNetwork, MetrowayNetwork metrowayNetwork, 
+            RailwayNetwork railwayNetwork, EntityStorage<AgentPolisEntity> entityStorage, AgentStorage agentStorage, 
+            VehicleStorage vehicleStorage, AgentPositionModel agentPositionModel, 
+            VehiclePositionModel vehiclePositionModel, AllNetworkNodes allNetworkNodes, 
+            SimulationCreator simulationCreator, DemandVisualisationLayer demandVisualisationLayer) {
+        super(pedestrianNetwork, bikewayNetwork, highwayNetwork, tramwayNetwork, metrowayNetwork, railwayNetwork, 
+                entityStorage, agentStorage, vehicleStorage, agentPositionModel, vehiclePositionModel, allNetworkNodes, 
+                simulationCreator);
+        this.demandVisualisationLayer = demandVisualisationLayer;
+    }
+
+    @Override
+    protected void initEntityLayers(Simulation simulation, Projection projection) {
+        VisManager.registerLayer(demandVisualisationLayer);
+    }
+    
+    
+    
+}

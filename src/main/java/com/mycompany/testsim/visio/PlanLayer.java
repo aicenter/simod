2c7d37d02605b596533e83525aecfe345b24f502
david.fiedler@agents.fel.cvut.cz <fido@FIDO-CVUT>
2016-10-13 16:51:35 +0200

    New plan layer

--- src/main/java/com/mycompany/testsim/visio/PlanLayer.java
+++ src/main/java/com/mycompany/testsim/visio/PlanLayer.java
@@ -5,12 +5,43 @@
  */
 package com.mycompany.testsim.visio;
 
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import cz.agents.agentpolis.simmodel.entity.AgentPolisEntity;
+import cz.agents.agentpolis.simmodel.environment.model.EntityStorage;
 import cz.agents.alite.vis.layer.AbstractLayer;
+import java.awt.Graphics2D;
 
 /**
  *
  * @author fido
  */
+@Singleton
 public class PlanLayer extends AbstractLayer{
+    
+    private final EntityStorage<AgentPolisEntity> entityStorage;
+    
+    private 
+
+    
+    
+    
+    @Inject
+    public PlanLayer(EntityStorage entityStorage) {
+        this.entityStorage = entityStorage;
+    }
+    
+    
+    
+
+    @Override
+    public void paint(Graphics2D canvas) {
+        for (AgentPolisEntity entity : entityStorage) {
+            
+        }
+    }
+    
+    
+    
     
 }

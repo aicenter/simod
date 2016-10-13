4944acc0415a3a7d5e716b2f7a8c4bc365c6309b
david.fiedler@agents.fel.cvut.cz <fido@FIDO-CVUT>
2016-10-13 16:27:33 +0200

    reflection of the changes in ShortestPathPlanner

---
+++ src/main/java/com/mycompany/testsim/visio/PlanLayer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testsim.visio;
+
+import cz.agents.alite.vis.layer.AbstractLayer;
+
+/**
+ *
+ * @author fido
+ */
+public class PlanLayer extends AbstractLayer{
+    
+}

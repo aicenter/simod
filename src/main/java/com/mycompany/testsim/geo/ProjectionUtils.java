7f02f7a0d654e7cf8ea00e4aaced4c8dbcd6516e
david.fiedler@agents.fel.cvut.cz <fido@FIDO-CVUT>
2016-11-04 12:27:33 +0100

    --
    Former-commit-id: 12be1e01288cd5a0264a47651b6dbcf27d197f32

---
+++ src/main/java/com/mycompany/testsim/geo/ProjectionUtils.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testsim.geo;
+
+import cz.agents.basestructures.GPSLocation;
+
+/**
+ *
+ * @author fido
+ */
+//public class ProjectionUtils {
+//    public static int utmToSrid(GPSLocation gpsLocation){
+//        
+//        int baseSrid;
+//    
+//        if(gpsLocation.getLatitude() < 0){
+//            // south hemisphere
+//            baseSrid = 32700;
+//        }
+//        else{
+//            // north hemisphere or on equator
+//            baseSrid = 32600;
+//        }
+//
+//        baseSrid = (int) (baseSrid + Math.floor((gpsLocation.getLongitude() + 186) / 6));
+//        if(gpsLocation.getLongitude() == 180){
+//                 out_srid := base_srid + 60;
+//        }
+//
+//         --- TODO: consider special cases around norway etc.
+//        return baseSrid;
+//    }
+//}

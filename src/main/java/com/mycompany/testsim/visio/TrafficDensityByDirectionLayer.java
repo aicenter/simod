1cc27e7b807517724632f52029823743a3853760
bousazde <bousazde@fel.cvut.cz>
2016-11-15 06:12:14 +0100

    Functional object draft
    Former-commit-id: 0662eaa1f43b4ba950752eebe28c24bf51dd00ba

--- src/main/java/com/mycompany/testsim/visio/TrafficDensityByDirectionLayer.java
+++ src/main/java/com/mycompany/testsim/visio/TrafficDensityByDirectionLayer.java
@@ -15,6 +15,7 @@ import cz.agents.agentpolis.simmodel.environment.model.citymodel.transportnetwor
 import cz.agents.agentpolis.simulator.visualization.visio.PositionUtil;
 import cz.agents.alite.vis.Vis;
 import cz.agents.alite.vis.layer.AbstractLayer;
+import cz.agents.basestructures.Edge;
 import cz.agents.basestructures.Graph;
 import edu.mines.jtk.awt.ColorMap;
 
@@ -26,9 +27,13 @@ import java.awt.Rectangle;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import javax.vecmath.Point2d;
 
 /**
+ * Layer that shows traffic on edges. Two-way edges are split for each direction.
  * @author Zdenek Bousa
  */
 @Singleton
@@ -49,9 +54,15 @@ public class TrafficDensityByDirectionLayer extends AbstractLayer {
 
     private Dimension dimension;
 
-    private HashMap<SimulationEdge, Line2D> tableOfVisibleEdges;
+    private List<SimulationEdge> tableOfVisibleEdges;
 
     private Point2d lastPoint = new Point2d(0, 0);
+
+    private Map<Edge, Edge> twoWayEdges;
+
+    private Map<Edge, SimulationEdge> edgeMapping;
+
+    private Map<Edge, Line2D> edgePosition;
 
 
     /**
@@ -81,6 +92,12 @@ public class TrafficDensityByDirectionLayer extends AbstractLayer {
             lastPoint = point;
             dimension = dimTemp;
             refreshListOfVisibleEdges();
+
+            if (twoWayEdges == null) {
+                createTwoWayEdgesList();
+            }
+
+            refreshEdgesPosition();
         }
 
         canvas.setStroke(new BasicStroke(EDGE_WIDTH));
@@ -88,9 +105,9 @@ public class TrafficDensityByDirectionLayer extends AbstractLayer {
 
         AllEdgesLoad allEdgesLoad = allEdgesLoadProvider.get();
 
-        for (SimulationEdge edge : tableOfVisibleEdges.keySet()) {
-            canvas.setColor(getColorForEdge(allEdgesLoad, edge));
-            canvas.draw(tableOfVisibleEdges.get(edge));
+        for (Edge edge : edgePosition.keySet()) {
+            canvas.setColor(getColorForEdge(allEdgesLoad, edgeMapping.get(edge)));
+            canvas.draw(edgePosition.get(edge));
         }
     }
 
@@ -100,19 +117,96 @@ public class TrafficDensityByDirectionLayer extends AbstractLayer {
         return colorMap.getColor(loadPerLength);
     }
 
+    /**
+     * Refresh or create list of edges that intersects with Rectangle(dimension)
+     */
     private void refreshListOfVisibleEdges() {
-        tableOfVisibleEdges = new HashMap<>();
+        //TODO: change table of visible edges to list
+        tableOfVisibleEdges = new LinkedList<>();
         Rectangle2D drawingRectangle = new Rectangle(dimension);
+
+        // will refresh twoWayEdges
+        twoWayEdges = null;
 
         for (SimulationEdge edge : graph.getAllEdges()) {
             Point2d from = positionUtil.getCanvasPosition(graph.getNode(edge.fromId));
             Point2d to = positionUtil.getCanvasPosition(graph.getNode(edge.toId));
-
             Line2D line2d = new Line2D.Double(from.x, from.y, to.x, to.y);
+
             if (line2d.intersects(drawingRectangle)) {
-                tableOfVisibleEdges.put(edge, line2d);
+                tableOfVisibleEdges.add(edge);
             }
         }
+    }
+
+    /**
+     * Provides easy mapping between Edge and SimulationEdge
+     * Contains only visible edges because of the listOfVisibleEdges
+     */
+    private void refreshEdgeMapping() {
+        edgeMapping = new HashMap<>();
+
+        for (SimulationEdge edge : tableOfVisibleEdges) {
+            edgeMapping.put(edge, edge);
+        }
+    }
+
+    /**
+     * Create Map of edges, where one-way edges have null value, two-eay edges have its partner edge in opposite
+     * direction as an value.
+     */
+    private void createTwoWayEdgesList() {
+        refreshEdgeMapping();
+        twoWayEdges = new HashMap<>();
+
+        for (Edge edge : edgeMapping.keySet()) {
+            Edge edgeOpposite = new Edge(edge.getToId(), edge.fromId, edge.length);
+            if (twoWayEdges.containsKey(edgeOpposite)) {
+                twoWayEdges.replace(edgeOpposite, edge);
+                twoWayEdges.put(edge, edgeOpposite);
+            } else {
+                twoWayEdges.put(edge, null);
+            }
+        }
+    }
+
+    /**
+     * Refresh all pairs Edge-Line2D in edgePosition. It uses twoWayEdges map.
+     */
+    private void refreshEdgesPosition() {
+        edgePosition = new HashMap<>();
+
+        for (Edge edge : twoWayEdges.keySet()) {
+            Edge edge2 = twoWayEdges.get(edge);
+            if (twoWayEdges.get(edge) != null && (!edgePosition.containsKey(edge) || !edgePosition.containsKey(edge2))) {
+                calculateTwoWayEdgesPosition(edge, edge2);
+            } else if (twoWayEdges.get(edge) == null) {
+                Point2d from = positionUtil.getCanvasPosition(graph.getNode(edge.fromId));
+                Point2d to = positionUtil.getCanvasPosition(graph.getNode(edge.toId));
+
+                Line2D line2d = new Line2D.Double(from.x, from.y, to.x, to.y);
+                edgePosition.put(edge, line2d);
+            }
+        }
+    }
+
+    /**
+     * Calculate position for two way edge.
+     *
+     * @param edge1 one direction
+     * @param edge2 opposite direction
+     */
+    private void calculateTwoWayEdgesPosition(Edge edge1, Edge edge2) {
+        //TODO: put in edgePosition
+        Point2d from = positionUtil.getCanvasPosition(graph.getNode(edge1.fromId));
+        Point2d to = positionUtil.getCanvasPosition(graph.getNode(edge1.toId));
+        Line2D line2DE1 = new Line2D.Double(from.x, from.y, to.x, to.y);
+
+        from = positionUtil.getCanvasPosition(graph.getNode(edge2.fromId));
+        to = positionUtil.getCanvasPosition(graph.getNode(edge2.toId));
+        Line2D line2DE2 = new Line2D.Double(from.x, from.y, to.x, to.y);
+        edgePosition.put(edge1, line2DE1);
+        edgePosition.put(edge2, line2DE2);
     }
 
 }

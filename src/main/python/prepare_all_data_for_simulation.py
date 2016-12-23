3699e5fc3aa589e0091b43eae08d3514142c4c25
david.fiedler@agents.fel.cvut.cz <fido@FIDO-CVUT>
2016-12-23 17:30:28 +0100

    begining of the main preparation script
    Former-commit-id: 5f5bb5ffc4e43d02ede2cc96e1fcbf4c83250f11

--- src/main/python/prepare_all_data_for_simulation.py
+++ src/main/python/prepare_all_data_for_simulation.py
@@ -1,8 +1,14 @@
 from __future__ import print_function, division
 
-from datetime import datetime
+from scripts.printer import print_info
+from scripts.prague.prepare_trips import get_trips
 
-def print_info(info):
-    print("[{0}]: {1}".format(datetime.now().time(), info))
+from scripts.config_loader import config
+
 
 print_info("PREPARATIONS STARTED")
+
+get_trips()
+
+
+

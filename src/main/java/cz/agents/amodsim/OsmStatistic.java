a4990fee50780ea10e1af340620b118fe1e79e7a
David Fiedler <david.fiedler@agents.fel.cvut.cz>
2017-02-07 18:13:39 +0100

    osm map statistics added
    Former-commit-id: c14d6e30ac14f1bc92a1819de540dda6299937a7

---
+++ src/main/java/cz/agents/amodsim/OsmStatistic.java
@@ -0,0 +1,24 @@
+
+package cz.agents.amodsim;
+
+import cz.agents.amodsim.config.Config;
+import cz.agents.basestructures.Graph;
+import cz.agents.multimodalstructures.edges.RoadEdge;
+import cz.agents.multimodalstructures.nodes.RoadNode;
+import java.io.File;
+
+/**
+ *
+ * @author fido
+ */
+public class OsmStatistic {
+    public static void main(String[] args) {
+        Config config = new Configuration().load();
+        
+        Graph<RoadNode, RoadEdge> highwayGraph = OsmUtil.getHigwayGraph(new File(config.mapFilePath), config.srid);
+        
+        System.out.println("Edges total: " + highwayGraph.getAllEdges().size());
+        
+        System.out.println("Nodes total: " + highwayGraph.getAllNodes().size());
+    }
+}

e68a77e00e81203e65c0ac1c925d690c81dd65fc
theprochy <dvdprochzka2@gmail.com>
2018-08-13 11:15:59 +0200

    Minor changes of the screen recording implementation.
    Former-commit-id: 1eeda35fa0dbd703b42a164a6d0710bf0c58f8b8

--- src/main/java/cz/cvut/fel/aic/amodsim/visio/DemandLayer.java
+++ src/main/java/cz/cvut/fel/aic/amodsim/visio/DemandLayer.java
@@ -29,89 +29,93 @@ import javax.vecmath.Point2d;
  */
 @Singleton
 public class DemandLayer extends ClickableEntityLayer<DemandAgent>  {
-    
-    private static final Color DEMAND_COLOR = Color.RED;
-	
+
+	private static final Color DEMAND_COLOR = Color.RED;
+
 	private static final Color DROPPED_COLOR = Color.MAGENTA;
-    
-    private static final int SIZE = 1;
 
-    private static final int TRANSFORMABLE_SIZE = 4;
-	
+	private static final int SIZE = 1;
+
+	private static final int TRANSFORMABLE_SIZE = 4;
+
 	private static final Double TEXT_MARGIN_BOTTOM = 5.0;
-    
-    private static final Color TEXT_BACKGROUND_COLOR = Color.WHITE;
-	
-	
-	
-	
+
+	private static final Color TEXT_BACKGROUND_COLOR = Color.WHITE;
+
+
+
+
 	protected final Set<DemandAgent> demandsWithPrintedInfo;
-	
-	
-	
-    
-    
-    @Inject
-    public DemandLayer(DemandStorage demandStorage, AgentpolisConfig agentpolisConfig) {
-        super(demandStorage, agentpolisConfig);
+
+
+
+
+
+	@Inject
+	public DemandLayer(DemandStorage demandStorage, AgentpolisConfig agentpolisConfig) {
+		super(demandStorage, agentpolisConfig);
 		demandsWithPrintedInfo = new HashSet<>();
-    }
-	
-	
-	@Override
-    public void init(Vis vis) {
-        super.init(vis);
-        vis.addMouseListener(this);
-    }
+	}
 
-    
-    protected Point2d getDrivingAgentPosition(DemandAgent demandAgent){
-        return positionUtil.getCanvasPositionInterpolatedForVehicle(demandAgent.getTransportingEntity());
-    }
-    
-    protected Point2d getWaitingAgentPosition(DemandAgent demandAgent, Dimension drawingDimension){
-        return positionUtil.getCanvasPosition(demandAgent.getPosition());
-    }
-
-
-    @Override
-    protected Point2d getEntityPosition(DemandAgent demandAgent) {
-        if(demandAgent.getState() == DemandAgentState.DRIVING){
-            return getDrivingAgentPosition(demandAgent);
-        }
-        else{
-            return getWaitingAgentPosition(demandAgent, dim);
-        }
-    }
 
 	@Override
-	protected Point2d getEntityPositionInTime(DemandAgent entity, long time) {
-		if(entity.getState() == DemandAgentState.DRIVING){
-			return getDrivingAgentPosition(entity);
+	public void init(Vis vis) {
+		super.init(vis);
+		vis.addMouseListener(this);
+	}
+
+
+	protected Point2d getDrivingAgentPosition(DemandAgent demandAgent){
+		return positionUtil.getCanvasPositionInterpolatedForVehicle(demandAgent.getTransportingEntity());
+	}
+
+	protected Point2d getDrivingAgentPositionInTime(DemandAgent demandAgent, long time){
+		return positionUtil.getCanvasPositionInterpolatedForVehicleInTime(demandAgent.getTransportingEntity(), time);
+	}
+
+	protected Point2d getWaitingAgentPosition(DemandAgent demandAgent, Dimension drawingDimension){
+		return positionUtil.getCanvasPosition(demandAgent.getPosition());
+	}
+
+
+	@Override
+	protected Point2d getEntityPosition(DemandAgent demandAgent) {
+		if(demandAgent.getState() == DemandAgentState.DRIVING){
+			return getDrivingAgentPosition(demandAgent);
 		}
 		else{
-			return getWaitingAgentPosition(entity, dim);
+			return getWaitingAgentPosition(demandAgent, dim);
 		}
 	}
 
 	@Override
-    protected Color getEntityDrawColor(DemandAgent demandAgent) {
+	protected Point2d getEntityPositionInTime(DemandAgent demandAgent, long time) {
+		if(demandAgent.getState() == DemandAgentState.DRIVING){
+			return getDrivingAgentPositionInTime(demandAgent, time);
+		}
+		else{
+			return getWaitingAgentPosition(demandAgent, dim);
+		}
+	}
+
+	@Override
+	protected Color getEntityDrawColor(DemandAgent demandAgent) {
 		if(demandAgent.isDropped()){
 			return DROPPED_COLOR;
 		}
-		
-        return DEMAND_COLOR;
-    }
 
-    @Override
-    protected int getEntityTransformableRadius(DemandAgent demandAgent) {
-        return SIZE;
-    }
+		return DEMAND_COLOR;
+	}
 
-    @Override
-    protected double getEntityStaticRadius(DemandAgent demandAgent) {
-        return (double) SIZE;
-    }
+	@Override
+	protected int getEntityTransformableRadius(DemandAgent demandAgent) {
+		return SIZE;
+	}
+
+	@Override
+	protected double getEntityStaticRadius(DemandAgent demandAgent) {
+		return (double) SIZE;
+	}
 
 	@Override
 	protected void processClick(DemandAgent nearestEntity) {
@@ -125,7 +129,7 @@ public class DemandLayer extends ClickableEntityLayer<DemandAgent>  {
 
 	@Override
 	protected void drawEntity(DemandAgent demandAgent, Point2d entityPosition, Graphics2D canvas, Dimension dim) {
-		super.drawEntity(demandAgent, entityPosition, canvas, dim); 
+		super.drawEntity(demandAgent, entityPosition, canvas, dim);
 		if(demandsWithPrintedInfo.contains(demandAgent)){
 			double radius = getRadius(demandAgent);
 
@@ -134,17 +138,17 @@ public class DemandLayer extends ClickableEntityLayer<DemandAgent>  {
 			int x2 = (int) (entityPosition.getX() + radius);
 			int y2 = (int) (entityPosition.getY() + radius);
 			if (x2 > 0 && x1 < dim.width && y2 > 0 && y1 < dim.height) {
-				VisioUtils.printTextWithBackgroud(canvas, demandAgent.getId(), 
-						new Point((int) (x1 + TEXT_MARGIN_BOTTOM), y1 + (y2 - y1) / 2), Color.BLACK, 
+				VisioUtils.printTextWithBackgroud(canvas, demandAgent.getId(),
+						new Point((int) (x1 + TEXT_MARGIN_BOTTOM), y1 + (y2 - y1) / 2), Color.BLACK,
 						TEXT_BACKGROUND_COLOR);
 			}
 		}
 	}
-	
+
 	@Override
-	protected void drawEntities(ArrayList<DemandAgent> demandAgents, Point2d entityPosition, Graphics2D canvas, 
-			Dimension dim) {
-        super.drawEntities(demandAgents, entityPosition, canvas, dim);
+	protected void drawEntities(ArrayList<DemandAgent> demandAgents, Point2d entityPosition, Graphics2D canvas,
+								Dimension dim) {
+		super.drawEntities(demandAgents, entityPosition, canvas, dim);
 		if(demandsWithPrintedInfo.contains(demandAgents.get(0))){
 			double radius = getRadius(demandAgents.get(0));
 
@@ -154,15 +158,11 @@ public class DemandLayer extends ClickableEntityLayer<DemandAgent>  {
 			int y2 = (int) (entityPosition.getY() + radius);
 			if (x2 > 0 && x1 < dim.width && y2 > 0 && y1 < dim.height) {
 
-				VisioUtils.printTextWithBackgroud(canvas, demandAgents.get(0).getId(), 
-							new Point((int) (x1 + TEXT_MARGIN_BOTTOM), y1 + (y2 - y1) / 2), Color.BLACK, 
-							TEXT_BACKGROUND_COLOR);
+				VisioUtils.printTextWithBackgroud(canvas, demandAgents.get(0).getId(),
+						new Point((int) (x1 + TEXT_MARGIN_BOTTOM), y1 + (y2 - y1) / 2), Color.BLACK,
+						TEXT_BACKGROUND_COLOR);
 			}
 		}
-    }
-	
-	
-    
-	
-    
+	}
+
 }

c61ca8a4e31a4b6dbf51a47ce797147e66ed2ce5
F.I.D.O. <david.fido.fiedler@gmail.com>
2019-05-21 17:48:18 +0200

    license header update
    Former-commit-id: 1a380a399f3240534d8448f62f8e445c8ad5c910

--- src/main/java/cz/cvut/fel/aic/amodsim/entity/OnDemandVehicleStationFactory.java
+++ src/main/java/cz/cvut/fel/aic/amodsim/entity/OnDemandVehicleStationFactory.java
@@ -1,74 +1,18 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package cz.cvut.fel.aic.amodsim.entity;
-//
-//import com.google.inject.Inject;
-//import com.google.inject.Singleton;
-//import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.NearestElementUtils;
-//import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.elements.SimulationNode;
-//import cz.cvut.fel.aic.agentpolis.simulator.visualization.visio.VisioPositionUtil;
-//import cz.cvut.fel.aic.alite.common.event.EventProcessor;
-//import cz.cvut.fel.aic.amodsim.StationsDispatcher;
-//import cz.cvut.fel.aic.amodsim.config.AmodsimConfig;
-//import cz.cvut.fel.aic.amodsim.entity.vehicle.OnDemandVehicleFactorySpec;
-//import cz.cvut.fel.aic.amodsim.storage.OnDemandVehicleStorage;
-//import cz.cvut.fel.aic.amodsim.storage.OnDemandvehicleStationStorage;
-//import cz.cvut.fel.aic.geographtools.util.Transformer;
-//import java.util.Map;
-//
-///**
-// *
-// * @author fido
-// */
-//
-//@Singleton
-//public class OnDemandVehicleStationFactory {
-//	
-//	private final AmodsimConfig config;
-//	
-//	private final EventProcessor eventProcessor;
-//	
-//	private final OnDemandVehicleFactorySpec onDemandVehicleFactory;
-//	
-//	private final NearestElementUtils nearestElementUtils;
-//	
-//	private final OnDemandvehicleStationStorage onDemandVehicleStationStorage;
-//	
-//	private final OnDemandVehicleStorage onDemandVehicleStorage;
-//	
-//	private final Transformer transformer;
-//	
-//	private final VisioPositionUtil positionUtil;
-//	
-//	private final StationsDispatcher onDemandVehicleStationsCentral;
-//
-//	@Inject
-//	public OnDemandVehicleStationFactory(AmodsimConfig config, EventProcessor eventProcessor, OnDemandVehicleFactorySpec 
-//			onDemandVehicleFactory, NearestElementUtils nearestElementUtils, OnDemandvehicleStationStorage 
-//					onDemandVehicleStationStorage, OnDemandVehicleStorage onDemandVehicleStorage,
-//					Transformer transformer, VisioPositionUtil positionUtil, 
-//					StationsDispatcher onDemandVehicleStationsCentral) {
-//		this.config = config;
-//		this.eventProcessor = eventProcessor;
-//		this.onDemandVehicleFactory = onDemandVehicleFactory;
-//		this.nearestElementUtils = nearestElementUtils;
-//		this.onDemandVehicleStationStorage = onDemandVehicleStationStorage;
-//		this.onDemandVehicleStorage = onDemandVehicleStorage;
-//		this.transformer = transformer;
-//		this.positionUtil = positionUtil;
-//		this.onDemandVehicleStationsCentral = onDemandVehicleStationsCentral;
-//	}
-//	
-//	
-//	
-//	
-//	public OnDemandVehicleStation create(String id, SimulationNode node, int initialVehicleCount){
-//		return new OnDemandVehicleStation(config, eventProcessor, onDemandVehicleFactory, nearestElementUtils,
-//				onDemandVehicleStationStorage, onDemandVehicleStorage, id, node, 
-//				initialVehicleCount, transformer, positionUtil, 
-//				onDemandVehicleStationsCentral);
-//	}
-//}
+/* 
+ * Copyright (C) 2019 Czech Technical University in Prague.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */

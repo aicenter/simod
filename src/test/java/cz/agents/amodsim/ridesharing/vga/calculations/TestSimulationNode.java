adaa4b8d329454744fffd58ae62f2da117d82e76
Martin Lukeš <lukesma8@fel.cvut.cz>
2019-07-22 16:52:34 +0200

    tests fixed
    Former-commit-id: 7b6fdbcbe769557c5dc37589fdbd8550c8abec5d

--- src/test/java/cz/agents/amodsim/ridesharing/vga/calculations/TestSimulationNode.java
+++ src/test/java/cz/agents/amodsim/ridesharing/vga/calculations/TestSimulationNode.java
@@ -14,7 +14,7 @@ import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.elements
 public class TestSimulationNode extends SimulationNode {
     
     public TestSimulationNode(int latE6, int lonE6) {
-        super(-1, -1, latE6, lonE6, -1, -1, -1);
+        super(-1, -1, latE6, lonE6, -1, -1, -1,-1);
     }
     
 }

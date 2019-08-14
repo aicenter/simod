33482ca6d9f68b3e59a81673afadd382618f555a
F.I.D.O. <david.fido.fiedler@gmail.com>
2019-08-14 11:43:18 +0200

    adj matrix computation: edge duration computation standardized
    Former-commit-id: 5e33195b9cc5a3885c82c2584e645a2411359b6a

--- python/amodsim/compute_adjectancy_matrix.py
+++ python/amodsim/compute_adjectancy_matrix.py
@@ -9,9 +9,11 @@ def compute_traveltime_from_edge(edge: dict):
 	:param edge: edge disctionary as loaded from geojson.
 	:return: Edge traveltime in miliseconds.
 	"""
-	distance = edge['properties']['length']
-	velocity = min(compute_traveltime_from_edge.vehicle_velocity, edge['properties']['maxspeed'] / 3.6)
-	return int(round(distance / velocity * 1000))
+	distance_cm = edge['properties']['length']
+	posted_speed_cm_per_second = int(round(edge['properties']['maxspeed'] / 3.6 * 1E2))
+	vehicle_max_speed_cm_per_second = compute_traveltime_from_edge.vehicle_velocity * 1E2
+	velocity_cm_per_s = min(posted_speed_cm_per_second, vehicle_max_speed_cm_per_second)
+	return int(round(distance_cm / velocity_cm_per_s * 1E3))
 
 
 compute_traveltime_from_edge.vehicle_velocity = config.vehicle_speed_in_meters_per_second

b005fd06a8be6e774c1d1f577df1f7c9370fa755
F.I.D.O. <david.fido.fiedler@gmail.com>
2019-08-23 13:37:50 +0200

    ih runner updated
    Former-commit-id: 5c2e0bd48e76ad72e608e0c10704fdd03165f94e

--- rci_launchers/ih.sh
+++ rci_launchers/ih.sh
@@ -2,4 +2,5 @@
 PATH=$PATH:/home/fiedlda1/apache-maven-3.6.1/bin/
 ml Java
 ml Gurobi
+mvn compile
 mvn exec:exec -Dexec.executable=java -Dexec.args="-classpath %classpath -Xmx30g cz.cvut.fel.aic.amodsim.OnDemandVehiclesSimulation /home/fiedlda1/Amodsim/local_config_files/RCI/ih.cfg" -Dfile.encoding=UTF-8

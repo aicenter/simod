e73388b54c982730593327c526ecf043123d5dc3
F.I.D.O. <david.fido.fiedler@gmail.com>
2019-08-23 15:48:50 +0200

    RCI main launcher update
    Former-commit-id: 10e937db25a96e46a28710a7ef767ad7050cf7b5

--- run_all_rci.sh
+++ run_all_rci.sh
@@ -1,8 +1,8 @@
-sbatch --mem=25 -t 60 -n30 -o /home/fiedlda1/Amodsim/log/ih.log /home/fiedlda1/Amodsim/rci_lounchers/ih.sh
-sbatch --mem=25 -t 60 -n30 -o /home/fiedlda1/Amodsim/log/ih-sw.log /home/fiedlda1/Amodsim/rci_lounchers/ih-sw.sh
-sbatch --mem=25 -t 60 -n30 -o /home/fiedlda1/Amodsim/log/ih-nr.log /home/fiedlda1/Amodsim/rci_lounchers/ih-nr.sh
-sbatch --mem=25 -t 60 -n30 -o /home/fiedlda1/Amodsim/log/ih-nr-sw.log /home/fiedlda1/Amodsim/rci_lounchers/ih-nr-sh.sh
-sbatch --mem=75 -t 1500 -n30 -p longjobs -o /home/fiedlda1/Amodsim/log/vga.log /home/fiedlda1/Amodsim/rci_lounchers/vga.sh
-sbatch --mem=75 -t 1500 -n30 -p longjobs -o /home/fiedlda1/Amodsim/log/vga-sw.log /home/fiedlda1/Amodsim/rci_lounchers/vga-sw.sh
-sbatch --mem=75 -t 600 -n30 -o /home/fiedlda1/Amodsim/log/vga-lim.log /home/fiedlda1/Amodsim/rci_lounchers/vga-lim.sh
-sbatch --mem=75 -t 600 -n30 -o /home/fiedlda1/Amodsim/log/vga-lim-sw.log /home/fiedlda1/Amodsim/rci_lounchers/vga-lim-sw.sh
+sbatch --mem=25 -t 60 -n30 -o /home/fiedlda1/Amodsim/log/ih.log /home/fiedlda1/Amodsim/rci_launchers/ih.sh
+sbatch --mem=25 -t 60 -n30 -o /home/fiedlda1/Amodsim/log/ih-sw.log /home/fiedlda1/Amodsim/rci_launchers/ih-sw.sh
+sbatch --mem=25 -t 60 -n30 -o /home/fiedlda1/Amodsim/log/ih-nr.log /home/fiedlda1/Amodsim/rci_launchers/ih-nr.sh
+sbatch --mem=25 -t 60 -n30 -o /home/fiedlda1/Amodsim/log/ih-nr-sw.log /home/fiedlda1/Amodsim/rci_launchers/ih-nr-sh.sh
+sbatch --mem=75 -t 1500 -n30 -p longjobs -o /home/fiedlda1/Amodsim/log/vga.log /home/fiedlda1/Amodsim/rci_launchers/vga.sh
+sbatch --mem=75 -t 1500 -n30 -p longjobs -o /home/fiedlda1/Amodsim/log/vga-sw.log /home/fiedlda1/Amodsim/rci_launchers/vga-sw.sh
+sbatch --mem=75 -t 600 -n30 -o /home/fiedlda1/Amodsim/log/vga-lim.log /home/fiedlda1/Amodsim/rci_launchers/vga-lim.sh
+sbatch --mem=75 -t 600 -n30 -o /home/fiedlda1/Amodsim/log/vga-lim-sw.log /home/fiedlda1/Amodsim/rci_launchers/vga-lim-sw.sh

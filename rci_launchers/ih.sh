a1da6bbe1f9e3883102910c2155f2fb4f8b49f74
F.I.D.O. <david.fido.fiedler@gmail.com>
2019-08-23 15:58:42 +0200

    launchers update
    Former-commit-id: 3c7eb1ae3a7776dc5fd3a5f6ba91e980499f6834

--- rci_launchers/ih.sh
+++ rci_launchers/ih.sh
@@ -2,4 +2,4 @@
 PATH=$PATH:/home/fiedlda1/apache-maven-3.6.1/bin/
 ml Java
 ml Gurobi
-mvn exec:exec -Dexec.executable=java -Dexec.args="-classpath %classpath -Xmx30g cz.cvut.fel.aic.amodsim.OnDemandVehiclesSimulation /home/fiedlda1/Amodsim/local_config_files/RCI/ih.cfg" -Dfile.encoding=UTF-8
+mvn exec:exec -Dexec.executable=java -Dexec.args="-classpath %classpath -Xmx25g cz.cvut.fel.aic.amodsim.OnDemandVehiclesSimulation /home/fiedlda1/Amodsim/local_config_files/RCI/ih.cfg" -Dfile.encoding=UTF-8

dc137d93ee1ad38a7bccf0e32bc322c3cd3cc0a2
F.I.D.O. <david.fido.fiedler@gmail.com>
2019-09-16 13:55:12 +0200

    lim30 launcher, scale added to stations positions map
    Former-commit-id: 669a3ff2eefd30ab6c3e0fd2dd2d84683e808b91

--- python/amodsim/station_positions_map.py
+++ python/amodsim/station_positions_map.py
@@ -3,12 +3,15 @@ from amodsim.init import config
 import matplotlib.pyplot as plt
 import roadmaptools.inout
 import roadmaptools.plotting
+import roadmaptools.utm
+import matplotlib.font_manager as fm
 
+from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 from roadmaptools.printer import print_info
 from roadmaptools.graph import RoadGraph
 
 
-fig, axis = plt.subplots(1, 1, figsize=(4, 3))
+fig, axis = plt.subplots(1, 1, figsize=(8, 6))
 fig.subplots_adjust(wspace=0.01)
 
 axis.set_xticklabels([])
@@ -32,13 +35,34 @@ for item in nodes['features']:
 	node_dict[item['properties']['index']] = item
 x_list = []
 y_list = []
+projection = None
 for station in stations:
 	index = int(station[0])
-	x_list.append(node_dict[index]['geometry']['coordinates'][0])
-	y_list.append(node_dict[index]['geometry']['coordinates'][1])
-axis.scatter(x_list, y_list, edgecolors='red', facecolors='white', marker='o', s=20, zorder=2)
+
+	if not projection:
+		projection = roadmaptools.utm.TransposedUTM.from_gps(node_dict[index]['geometry']['coordinates'][1],
+															 node_dict[index]['geometry']['coordinates'][0])
+
+	coords = roadmaptools.utm.wgs84_to_utm(node_dict[index]['geometry']['coordinates'][1],
+										   node_dict[index]['geometry']['coordinates'][0])
+
+	x_list.append(coords[0])
+	y_list.append(coords[1])
+axis.scatter(x_list, y_list, edgecolors='red', facecolors='white', marker='o', s=40, zorder=2)
 axis.set_xlim(min(x_list), max(x_list))
 axis.set_ylim(min(y_list), max(y_list))
+
+# scale bar
+scalebar = AnchoredSizeBar(axis.transData,
+                           5000, '5 km', 'lower right',
+                           pad=0.3,
+                           color='black',
+                           frameon=True,
+                           size_vertical=1,
+							borderpad=0.5,
+                           fontproperties=fm.FontProperties(size=10))
+
+axis.add_artist(scalebar)
 
 plt.savefig(config.images.stations, bbox_inches='tight', transparent=True, pad_inches=0.0)
 

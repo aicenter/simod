5ec6a8aac76249787bc91e1e153defb31bf1ccbb
F.I.D.O. <david.fido.fiedler@gmail.com>
2020-03-26 20:43:36 +0100

    python: experiments title in figures changed (VGA optimal), Former-commit-id: cc8cb1e837886a60460edef01d5819e6cededadf

--- python/amodsim/statistics/comparisons/density_map_scenario_comparison.py
+++ python/amodsim/statistics/comparisons/density_map_scenario_comparison.py
@@ -17,15 +17,15 @@ matplotlib.rcParams.update({'font.size': FONT_SIZE})
 
 edgePairs = edges.load_edge_pairs()
 
-# loads_1 = traffic_load.load_all_edges_load_history(config.comparison.experiment_1_dir + config.statistics.all_edges_load_history_file_name)
-# loads_2 = traffic_load.load_all_edges_load_history(config.comparison.experiment_2_dir + config.statistics.all_edges_load_history_file_name)
-# loads_3 = traffic_load.load_all_edges_load_history(config.comparison.experiment_3_dir + config.statistics.all_edges_load_history_file_name)
-# loads_4 = traffic_load.load_all_edges_load_history(config.comparison.experiment_4_dir + config.statistics.all_edges_load_history_file_name)
+loads_1 = traffic_load.load_all_edges_load_history(config.comparison.experiment_1_dir + config.statistics.all_edges_load_history_file_name)
+loads_2 = traffic_load.load_all_edges_load_history(config.comparison.experiment_2_dir + config.statistics.all_edges_load_history_file_name)
+loads_3 = traffic_load.load_all_edges_load_history(config.comparison.experiment_3_dir + config.statistics.all_edges_load_history_file_name)
+loads_4 = traffic_load.load_all_edges_load_history(config.comparison.experiment_4_dir + config.statistics.all_edges_load_history_file_name)
 
-loads_1 = traffic_load.load_all_edges_load_history(config.comparison.experiment_5_dir + config.statistics.all_edges_load_history_file_name)
-loads_2 = traffic_load.load_all_edges_load_history(config.comparison.experiment_6_dir + config.statistics.all_edges_load_history_file_name)
-loads_3 = traffic_load.load_all_edges_load_history(config.comparison.experiment_7_dir + config.statistics.all_edges_load_history_file_name)
-loads_4 = traffic_load.load_all_edges_load_history(config.comparison.experiment_8_dir + config.statistics.all_edges_load_history_file_name)
+# loads_1 = traffic_load.load_all_edges_load_history(config.comparison.experiment_5_dir + config.statistics.all_edges_load_history_file_name)
+# loads_2 = traffic_load.load_all_edges_load_history(config.comparison.experiment_6_dir + config.statistics.all_edges_load_history_file_name)
+# loads_3 = traffic_load.load_all_edges_load_history(config.comparison.experiment_7_dir + config.statistics.all_edges_load_history_file_name)
+# loads_4 = traffic_load.load_all_edges_load_history(config.comparison.experiment_8_dir + config.statistics.all_edges_load_history_file_name)
 
 
 # subplot_kw={"adjustable": 'datalim', "aspect": 1.0}
@@ -39,8 +39,8 @@ np.vectorize(trafic_density_map.set_axis_params)(axis)
 axis[0].set_xlabel("a) Present State")
 axis[1].set_xlabel("b) No Ridesharing")
 axis[2].set_xlabel("c) Insertion Heuristic")
-axis[3].set_xlabel("d) VGA")
-axis[4].set_xlabel("e) VGA runtime-limited")
+axis[3].set_xlabel("d) VGA (optimal)")
+axis[4].set_xlabel("e) VGA (limited)")
 
 print_info("Plotting Present State load")
 trafic_density_map.plot_edges_optimized(edgePairs, axis[0], loads_1[VehicleState.DRIVING_TO_TARGET_LOCATION.name])

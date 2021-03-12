a0c7e5dd3cf9a83bd531058b366ee4c3ed2cbd64
David Fiedler <david.fido.fiedler@gmail.com>
2021-03-12 12:46:02 +0100

    pnas config, decrease memory allocation in RCI VGA lim launcher
    Former-commit-id: 54d323444ddae4f411414c4ca7558b046764b9f8

--- rci_launchers/vga-lim-30ms.sh
+++ rci_launchers/vga-lim-30ms.sh
@@ -20,4 +20,4 @@
 PATH=$PATH:/home/fiedlda1/apache-maven-3.6.1/bin/
 ml Java
 ml Gurobi
-mvn exec:exec -Dexec.executable=java -Dexec.args="-classpath %classpath -Xmx50g cz.cvut.fel.aic.amodsim.OnDemandVehiclesSimulation /home/fiedlda1/Amodsim/local_config_files/RCI/vga-lim-30ms.cfg" -Dfile.encoding=UTF-8
+mvn exec:exec -Dexec.executable=java -Dexec.args="-classpath %classpath -Xmx35g cz.cvut.fel.aic.amodsim.OnDemandVehiclesSimulation /home/fiedlda1/Amodsim/local_config_files/RCI/vga-lim-30ms.cfg" -Dfile.encoding=UTF-8

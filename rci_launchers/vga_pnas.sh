e0d6c0884cf8a1b5896d2a315533fe327f45cbbc
David Fiedler <david.fido.fiedler@gmail.com>
2021-03-19 18:28:13 +0100

    pnas rci launcher fixed
    Former-commit-id: 9257eceb1813a3ee6d871e4dbaac2138188f2e57

--- rci_launchers/vga_pnas.sh
+++ rci_launchers/vga_pnas.sh
@@ -20,4 +20,4 @@
 PATH=$PATH:/home/fiedlda1/apache-maven-3.6.1/bin/
 ml Java
 ml Gurobi
-mvn exec:exec -Dexec.executable=java -Dexec.args="-classpath %classpath -Xmx30g cz.cvut.fel.aic.amodsim.OnDemandVehiclesSimulation /home/fiedlda1/Amodsim/local_config_files/RCI/vga_pnas.cfg" -Dfile.encoding=UTF-8
+mvn exec:exec -Dexec.executable=java -Dexec.args="-classpath %classpath -Xmx30g cz.cvut.fel.aic.amodsim.OnDemandVehiclesSimulation /home/fiedlda1/Amodsim/local_config_files/RCI/vga-pnas.cfg" -Dfile.encoding=UTF-8

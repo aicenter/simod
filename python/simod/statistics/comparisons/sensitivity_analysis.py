594f58af42d3f9f33959f16cd2e93e9a5d9628da
F.I.D.O. <david.fido.fiedler@gmail.com>
2021-08-19 17:04:24 +0200

    sensitivity analysis updated

--- python/simod/statistics/comparisons/sensitivity_analysis.py
+++ python/simod/statistics/comparisons/sensitivity_analysis.py
@@ -32,11 +32,19 @@ import simod.statistics.model.service as service
 import simod.statistics.comparisons.common as common
 
 
+def check_experiments(experiments: Dict[str, List[List[str]]]):
+    for experiment_set in experiments.values():
+        for method in experiment_set:
+            for exp_name in method:
+                experiment_dir = "{}/{}/".format(config.experiments_dir, exp_name)
+                if not os.path.exists(experiment_dir):
+                    raise Exception(f"experiment {experiment_dir} not found")
+
 
 def load_experiment_data(experiment_name: str, edge_data: pd.DataFrame):
+    experiment_dir = "{}/{}/".format(config.experiments_dir, experiment_name)
 
     # distance
-    experiment_dir = "{}/{}/".format(config.experiments_dir, experiment_name)
     transit_data = transit.load(experiment_dir)
     km_total_window = int(round(transit.get_total_distance(transit_data, edge_data, True) / 1000 / 100))
 
@@ -53,39 +61,38 @@ def load_experiment_data(experiment_name: str, edge_data: pd.DataFrame):
 
     return km_total_window, avg_time, mean_delay
 
-def check_experiments(experiments: Dict[str, List[List[str]]]):
-    for experiment_set in experiments.values():
-        for method in experiment_set:
-            for exp_name in method:
-                experiment_dir = "{}/{}/".format(config.experiments_dir, exp_name)
-                if not os.path.exists(experiment_dir):
-                    raise Exception(f"experiment {experiment_dir} not found")
-
 
 def load_experiments(experiments: Dict[str, List[List[str]]], edge_data: pd.DataFrame):
-    speed_data = []
-    tt_data = []
+    distance_data = []
+    comp_time_data = []
     delay_data = []
 
     for experiment_set in experiments.values():
         for method in experiment_set:
             for exp_name in method:
                 record = load_experiment_data(exp_name, edge_data)
-                speed_data.append(record[0])
-                tt_data.append(record[1])
+                distance_data.append(record[0])
+                comp_time_data.append(record[1])
                 delay_data.append(record[2])
 
-    speed_data = [rec / 1000 for rec in speed_data]
-    tt_data = [rec / 1000 for rec in tt_data]
+    speed_data = [rec / 1000 for rec in distance_data]
+    tt_data = [rec / 1000 for rec in comp_time_data]
 
-    return speed_data, tt_data, delay_data
+    return distance_data, comp_time_data, delay_data
 
 
 def plot_ax(xvalues, data: List[float], axis, experiment: common.Experiment):
     axis.plot(xvalues, data, marker=experiment.marker, linestyle='dashed', color=experiment.color, label=experiment.label)
 
 
-def plot_sensitivity_analysis(experiments: Dict[str, List[List[str]]], speed_data, tt_data, delay_data, x_values):
+def plot_sensitivity_analysis(
+        experiments: Dict[str, List[List[str]]],
+        distance_data,
+        comp_time_data,
+        delay_data,
+        x_values,
+        exp_set_name: str = "peak"
+):
     fig, axes = plt.subplots(3, 3, sharex='col')
     axes[0][0].set_ylabel("Avg. comp. \n time [s]")
     axes[1][0].set_ylabel("Total Distance \n" r"[km $\cdot 10^3$]")
@@ -104,12 +111,18 @@ def plot_sensitivity_analysis(experiments: Dict[str, List[List[str]]], speed_dat
         for method, experiment in zip(experiment_set, islice(common.Experiment, 3, 6)):
             to_index += len(method)
 
-            plot_ax(x_values_l[:len(method)], tt_data[from_index:to_index], axes[0][axis_index], experiment)
-            plot_ax(x_values_l[:len(method)], speed_data[from_index:to_index], axes[1][axis_index], experiment)
+            plot_ax(x_values_l[:len(method)], comp_time_data[from_index:to_index], axes[0][axis_index], experiment)
+            plot_ax(x_values_l[:len(method)], distance_data[from_index:to_index], axes[1][axis_index], experiment)
             plot_ax(x_values_l[:len(method)], delay_data[from_index:to_index], axes[2][axis_index], experiment)
 
             from_index = to_index
 
     axes[0][0].legend(loc=1, fontsize='small', borderaxespad=0.2)
-    plt.savefig(config.images.sensitivity_analysis, bbox_inches='tight', transparent=True, pad_inches=0.0, dpi=fig.dpi)
+    plt.savefig(
+        f"{config.images.images_dir}/sensitivity_analysis-{exp_set_name}.pdf",
+        bbox_inches='tight',
+        transparent=True,
+        pad_inches=0.0,
+        dpi=fig.dpi
+    )
 

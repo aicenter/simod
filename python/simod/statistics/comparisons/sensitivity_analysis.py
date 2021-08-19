bfb6540fb3723e860f419ef5013e461bb0a35e4c
David Fiedler <david.fido.fiedler@gmail.com>
2021-08-19 09:21:21 +0200

    sensitivitz analysis updated, simod import fixed in some python files

--- python/simod/statistics/comparisons/sensitivity_analysis.py
+++ python/simod/statistics/comparisons/sensitivity_analysis.py
@@ -16,27 +16,25 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
+from typing import List, Tuple, Dict
+import pandas as pd
+import matplotlib.pyplot as plt
+from itertools import islice
+import os.path
+
 from simod.init import config
 
-from typing import List, Tuple
 import roadmaptools.inout
 import simod.statistics.model.edges as edges
 import simod.statistics.model.transit as transit
 import simod.statistics.model.ridesharing as ridesharing
 import simod.statistics.model.service as service
-import matplotlib.pyplot as plt
-
-# delay_experiments = ["sw-vga-max_delay_3_min", "sw-vga", "sw-vga-max_delay_5_min", "sw-vga-max_delay_6_min"]
-# batch_experiments = ["sw-vga", "sw-vga-batch_60s", "sw-vga-batch_90s", "sw-vga-batch_120s"]
-
-delay_experiments = ["vga-max_delay_3_min", "vga", "vga-max_delay_5_min", "vga-max_delay_6_min", "vga-max_delay_7_min"]
-batch_experiments = ["vga", "vga-batch_60s", "vga-batch_90s", "vga-batch_120s"]
-
-loaded_edges = roadmaptools.inout.load_geojson(config.agentpolis.map_edges_filepath)
-edge_data = edges.make_data_frame(loaded_edges)
+import simod.statistics.comparisons.common as common
 
 
-def load_experiment_data(experiment_name: str):
+
+def load_experiment_data(experiment_name: str, edge_data: pd.DataFrame):
+
     # distance
     experiment_dir = "{}/{}/".format(config.experiments_dir, experiment_name)
     transit_data = transit.load(experiment_dir)
@@ -55,48 +53,63 @@ def load_experiment_data(experiment_name: str):
 
     return km_total_window, avg_time, mean_delay
 
-
-speed_data = []
-tt_data = []
-delay_data = []
-for exp_name in delay_experiments:
-    record = load_experiment_data(exp_name)
-    speed_data.append(record[0])
-    tt_data.append(record[1])
-    delay_data.append(record[2])
-
-for exp_name in batch_experiments:
-    record = load_experiment_data(exp_name)
-    speed_data.append(record[0])
-    tt_data.append(record[1])
-    delay_data.append(record[2])
-
-speed_data = [rec / 1000 for rec in speed_data]
-tt_data = [rec / 1000 for rec in tt_data]
+def check_experiments(experiments: Dict[str, List[List[str]]]):
+    for experiment_set in experiments.values():
+        for method in experiment_set:
+            for exp_name in method:
+                experiment_dir = "{}/{}/".format(config.experiments_dir, exp_name)
+                if not os.path.exists(experiment_dir):
+                    raise Exception(f"experiment {experiment_dir} not found")
 
 
-def plot_ax(xvalues, data: List[float], axis):
-    axis.plot(xvalues, data, marker='o', linestyle='dashed', color='black')
+def load_experiments(experiments: Dict[str, List[List[str]]], edge_data: pd.DataFrame):
+    speed_data = []
+    tt_data = []
+    delay_data = []
 
-x_values_delay = [3, 4, 5, 6]
-x_values_batch = [30, 60, 90, 120]
+    for experiment_set in experiments.values():
+        for method in experiment_set:
+            for exp_name in method:
+                record = load_experiment_data(exp_name, edge_data)
+                speed_data.append(record[0])
+                tt_data.append(record[1])
+                delay_data.append(record[2])
 
-fig, axes = plt.subplots(3, 2, sharey='row', sharex='col')
-axes[0][0].set_ylabel("Total Distance \n" r"[km $\cdot 10^3$]")
-axes[1][0].set_ylabel("Avg. comp. \n time [s]")
-axes[2][0].set_ylabel("Avg. delay [s]")
-axes[2][0].set_xlabel(r"Batch length [s]")
-axes[2][0].set_xticks(x_values_batch)
-axes[2][1].set_xlabel(r"Max delay [min]")
+    speed_data = [rec / 1000 for rec in speed_data]
+    tt_data = [rec / 1000 for rec in tt_data]
 
-plot_ax(x_values_batch, speed_data[4:], axes[0][0])
-plot_ax(x_values_delay, speed_data[:4], axes[0][1])
-plot_ax(x_values_batch, tt_data[4:], axes[1][0])
-plot_ax(x_values_delay, tt_data[:4], axes[1][1])
-plot_ax(x_values_batch, delay_data[4:], axes[2][0])
-plot_ax(x_values_delay, delay_data[:4], axes[2][1])
-
-plt.savefig(config.images.sensitivity_analysis, bbox_inches='tight', transparent=True, pad_inches=0.0, dpi=fig.dpi)
+    return speed_data, tt_data, delay_data
 
 
-plt.show()
+def plot_ax(xvalues, data: List[float], axis, experiment: common.Experiment):
+    axis.plot(xvalues, data, marker=experiment.marker, linestyle='dashed', color=experiment.color, label=experiment.label)
+
+
+def plot_sensitivity_analysis(experiments: Dict[str, List[List[str]]], speed_data, tt_data, delay_data, x_values):
+    fig, axes = plt.subplots(3, 3, sharex='col')
+    axes[0][0].set_ylabel("Avg. comp. \n time [s]")
+    axes[1][0].set_ylabel("Total Distance \n" r"[km $\cdot 10^3$]")
+    axes[2][0].set_ylabel("Avg. delay [s]")
+    axes[2][0].set_xlabel(r"Batch length [s]")
+    axes[2][0].set_xticks(x_values[0])
+    axes[2][1].set_xlabel(r"Max delay [min]")
+    axes[2][1].set_xticks(x_values[1])
+    axes[2][2].set_xlabel(r"Capacity")
+    axes[2][2].set_xticks(x_values[2])
+
+    to_index = 0
+    from_index = 0
+    for experiment_set, axis_index in zip(experiments.values(), [1, 0, 2]):
+        x_values_l = x_values[axis_index]
+        for method, experiment in zip(experiment_set, islice(common.Experiment, 3, 6)):
+            to_index += len(method)
+
+            plot_ax(x_values_l[:len(method)], tt_data[from_index:to_index], axes[0][axis_index], experiment)
+            plot_ax(x_values_l[:len(method)], speed_data[from_index:to_index], axes[1][axis_index], experiment)
+            plot_ax(x_values_l[:len(method)], delay_data[from_index:to_index], axes[2][axis_index], experiment)
+
+            from_index = to_index
+
+    axes[0][0].legend(loc=1, fontsize='small', borderaxespad=0.2)
+    plt.savefig(config.images.sensitivity_analysis, bbox_inches='tight', transparent=True, pad_inches=0.0, dpi=fig.dpi)
+

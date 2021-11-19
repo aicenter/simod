05c599c10a42706c97c0079f93cf29ad6afe6d89
Martin Bláha <blaham17@fel.cvut.cz>
2021-11-19 23:54:52 +0100

    created own versions of DARPSolver, HeuristicSolver, PFvehicle and PlanComputationRequests

---
+++ src/main/java/cz/cvut/fel/aic/simod/ridesharing/DARPSolverPFShared.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2021 Czech Technical University in Prague.
+ *
+ * This file is part of the SiMoD project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cvut.fel.aic.simod.ridesharing;
+
+import cz.cvut.fel.aic.simod.ridesharing.insertionheuristic.DriverPlan;
+import cz.cvut.fel.aic.simod.ridesharing.model.DefaultPlanComputationRequest;  //.DefaultPlanComputationRequestFactory;
+import cz.cvut.fel.aic.simod.ridesharing.model.PlanComputationRequestFreight;
+import cz.cvut.fel.aic.simod.ridesharing.model.PlanComputationRequestPeople;
+import cz.cvut.fel.aic.simod.traveltimecomputation.TravelTimeProvider;
+import cz.cvut.fel.aic.simod.statistics.content.RidesharingBatchStats;
+import cz.cvut.fel.aic.simod.storage.OnDemandVehicleStorage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+
+public abstract class DARPSolverPFShared {
+
+    protected final OnDemandVehicleStorage vehicleStorage;
+
+    protected final TravelTimeProvider travelTimeProvider;
+
+    protected final PlanCostProvider planCostProvider;
+
+    protected final DefaultPlanComputationRequest.DefaultPlanComputationRequestFactory requestFactory;
+
+    protected final List<RidesharingBatchStats> ridesharingStats;
+
+
+    protected RidesharingDispatcher ridesharingDispatcher;
+
+
+
+
+    public List<RidesharingBatchStats> getRidesharingStats() {
+        return ridesharingStats;
+    }
+
+    public void setDispatcher(RidesharingDispatcher ridesharingDispatcher){
+        this.ridesharingDispatcher = ridesharingDispatcher;
+    }
+
+
+
+
+
+    public DARPSolverPFShared(OnDemandVehicleStorage vehicleStorage, TravelTimeProvider travelTimeProvider,
+                      PlanCostProvider travelCostProvider, DefaultPlanComputationRequest.DefaultPlanComputationRequestFactory requestFactory) {
+        this.vehicleStorage = vehicleStorage;
+        this.travelTimeProvider = travelTimeProvider;
+        this.planCostProvider = travelCostProvider;
+        this.requestFactory = requestFactory;
+        ridesharingStats = new ArrayList<>();
+    }
+
+
+
+
+    public abstract Map<RideSharingOnDemandVehicle, DriverPlan> solve(List<PlanComputationRequestPeople> newPeopleRequests,
+                                                                      List<PlanComputationRequestPeople> waitingPeopleRequests,
+                                                                      List<PlanComputationRequestFreight> newFreightRequests,
+                                                                      List<PlanComputationRequestFreight> waitingFreightRequests);
+}
+
---
+++ src/main/java/cz/cvut/fel/aic/simod/ridesharing/model/PlanComputationRequestPeople.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2021 Czech Technical University in Prague.
+ *
+ * This file is part of the SiMoD project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cvut.fel.aic.simod.ridesharing.model;
+
+import cz.cvut.fel.aic.agentpolis.simmodel.environment.transportnetwork.elements.SimulationNode;
+import cz.cvut.fel.aic.simod.entity.DemandAgent;
+
+public interface PlanComputationRequestPeople {
+
+    @Override
+    public boolean equals(Object obj);
+
+
+    /**
+     * Returns max pickup time in seconds.
+     * @return max pickup time in seconds.
+     */
+    public int getMaxPickupTime();
+
+    /**
+     * Returns max dropoff time in seconds.
+     * @return max dropoff time in seconds.
+     */
+    public int getMaxDropoffTime();
+
+    /**
+     * Returns request origin time in seconds.
+     * @return Request origin time in seconds.
+     */
+    public int getOriginTime();
+
+    /**
+     * Returns min travel time in seconds.
+     * @return Min travel time in seconds.
+     */
+    public int getMinTravelTime();
+
+    public SimulationNode getFrom();
+
+    public SimulationNode getTo();
+
+    public boolean isOnboard();
+
+    public PlanActionPickup getPickUpAction();
+
+    public PlanActionDropoff getDropOffAction();
+
+    public DemandAgent getDemandAgent();
+
+    public int getId();
+
+    public void setOnboard(boolean onboard);
+}

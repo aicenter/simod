b58688e67d03067a350c00fba453361d519fec86
Martin Bláha <blaham17@fel.cvut.cz>
2022-01-04 01:22:37 +0100

    corrected checking for taxi availability

--- src/main/java/cz/cvut/fel/aic/simod/storage/OnDemandPFVehicleStorage.java
+++ src/main/java/cz/cvut/fel/aic/simod/storage/OnDemandPFVehicleStorage.java
@@ -1,2 +1,18 @@
-package cz.cvut.fel.aic.simod.storage;public class OnDemandPFVehicleStorage {
+package cz.cvut.fel.aic.simod.storage;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import cz.cvut.fel.aic.agentpolis.simmodel.environment.EntityStorage;
+import cz.cvut.fel.aic.simod.entity.vehicle.OnDemandVehicle;
+import cz.cvut.fel.aic.simod.ridesharing.peoplefreightsheuristic.OnDemandPFVehicle;
+
+@Singleton
+public class OnDemandPFVehicleStorage extends EntityStorage<OnDemandPFVehicle>
+{
+    @Inject
+    public OnDemandPFVehicleStorage()
+    {
+        super();
+    }
+
 }
